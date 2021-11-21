ea7961889504a46e8b1fc34400fdf6e3266222d4
Hanil Trivedi <trivedihanil@gmail.com>
2021-11-21 19:27:11 +0530

    enabled index creation and added index on email id.Created the query to insert the email based on unique values only from command line args.

--- mongodb/src/main/java/com/hanil/mongodb/MongodbApplication.java
+++ mongodb/src/main/java/com/hanil/mongodb/MongodbApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -12,21 +15,33 @@ import java.util.List;
 @SpringBootApplication
 public class MongodbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MongodbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbApplication.class, args);
+    }
 
-	@Bean
-	CommandLineRunner runner(StudentRepository repository){
-		return args -> {
+    @Bean
+    CommandLineRunner runner(StudentRepository repository, MongoTemplate mongoTemplate) {
+        return args -> {
 
-			Student student = new Student(
-					"Hanil","Trivedi","ht@ghgh.com",Gender.MALE ,new Address("India","126152",
-					"Bangalore"), List.of("Maths","science"), BigDecimal.TEN, LocalDateTime.now()
-			);
+            String email = "st@ghgh.com";
+            Student student = new Student(
+                    "Saurabh", "Trivedi", email, Gender.MALE, new Address("India", "126152",
+                    "Pune"), List.of("Sanskrit", "science", "Arts"), BigDecimal.TEN, LocalDateTime.now()
+            );
 
-			repository.insert(student);
-
-		};
-	}
+            Query query = new Query();
+            query.addCriteria(Criteria.where("email").is(email));
+            List<Student> students = mongoTemplate.find(query, Student.class);
+            if (students.size()>1){
+                throw new IllegalStateException("Found many students with email - "+email);
+            }
+            if(students.isEmpty()){
+                System.out.println("inserting student "+student);
+                repository.insert(student);
+            }
+            else {
+                System.out.println("already with same email id exists - "+email);
+            }
+        };
+    }
 }
--- mongodb/src/main/java/com/hanil/mongodb/Student.java
+++ mongodb/src/main/java/com/hanil/mongodb/Student.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
@@ -20,6 +21,7 @@ public class Student {
     private String id;
     private String firstName;
     private String lastName;
+    @Indexed(unique = true)
     private String email;
     private Gender gender;
     private Address address;
